a0a86c1e8b583ac2dbbe917396879fa24620b497
Omar Quintero <omar.quintero.ms@gmail.com>
2019-02-13 09:02:31 -0600

    Recorrido de Automata correcto

--- Compilador C/main.cpp
+++ Compilador C/main.cpp
@@ -12,28 +12,73 @@ void ReadFileLexico();
 void ReadFilePrograma();
 void ReadFilePalabrasReservadas();
 
+struct Token { public: string token; int estado; };
+
 vector<vector<vector<int>>> matrizLexico;
-vector<string> tokens;
+vector<Token> tokens;
 vector<string> palabras_reservadas;
 string programa;
 
 int* SiguienteEstado(int estadoActual, char caracter) {
 	int carac = -1;
 	carac = (caracter != '\n') ? (caracter != '\t') ? caracter - 32 + 3 : 2 : 1;
-	if (carac >= 98) return new int[1]{ -2 };
+	if ((carac >= 98)|| (carac < 0)) {  return new int[1]{ -2 }; }
 	
-	int pilaCount = (matrizLexico[estadoActual][carac]).size();
-	int pilaCountVacio = (matrizLexico[estadoActual][0]).size();
-	int* pilaEstados = new int[pilaCount + pilaCountVacio + 1];
-	for (int i = 0; i < pilaCount; i++) {
+	int pila = (matrizLexico[estadoActual][carac]).size();
+	int pilaVacio = (matrizLexico[estadoActual][0]).size();
+	int pilaVacioValid = 0;
+	int* pilaEstados = new int[pila + pilaVacio + 1];
+	for (int i = 0; i < pila; i++) {
 		pilaEstados[i + 1] = matrizLexico[estadoActual][carac][i];
 	}
-	for (int i = 0; i < pilaCountVacio; i++) {
-		pilaEstados[i + pilaCount + 1] = matrizLexico[estadoActual][0][i];
+	for (int i = 0; i < pilaVacio; i++) {
+		if (matrizLexico[estadoActual][0][i] != -1) {
+			pilaEstados[pilaVacioValid + pila + 1] = matrizLexico[estadoActual][0][i];
+			pilaVacioValid++;
+		}
 	}
-	pilaEstados[0] = pilaCount + pilaCountVacio;
+	pilaEstados[0] = pila + pilaVacioValid;
 
 	return pilaEstados;
+}
+
+bool ValidarEstadoToken(string texto, int estado, int exitStatus, int columna, int linea) {
+	int token = -1;
+	if (exitStatus == -1) {
+		for (int i = 0; i < tokens.size(); i++) {
+			if (estado == tokens[i].estado) {
+				token = i;
+				if (tokens[i].token == "IDENTIFICADOR") { //Si es identificdor, busco si es palabra reservada
+					for (int j = 0; j < palabras_reservadas.size(); j++) {
+						if (texto == palabras_reservadas[j]) tokens[i].token = "PALABRA_RESERVADA";
+					}
+				}
+			}
+		}
+		if (token != -1) cout << tokens[token].token << "(" << linea << ", " << columna << "): " << texto << endl;
+	} else if (exitStatus == -2) {
+		cout << "NO VALID : " << "(" << linea << ", " << columna << "): " << texto << endl;
+	}
+	return (token!=-1);
+}
+
+void AutomataLexico(string texto, int index, int estado, int columna, int linea) {
+	int* estados = SiguienteEstado(estado, programa[index]);
+	for (int k = 0; k < estados[0]; k++) {
+		if (index >= programa.length()-1) {	//Llegue al final, exitStatus es -1 y el estado es al que hubiera ido
+			ValidarEstadoToken(texto + programa[index], estados[k + 1], -1, columna, linea);
+		} else if (estados[k + 1] < 0) {			//Si el estado es menor a 0, verifico estado final y comienzo desde ahi
+			if(ValidarEstadoToken(texto, estado, estados[k + 1], columna, linea))
+				AutomataLexico("", index, 0, columna, linea);
+		}else {										//Si no es menor a 0, me sigo moviendo
+			columna++; if (programa[index] == '\n') { linea++; columna = 0; } //Llevo el conteo de todo
+			if (estados[k + 1] == 0) {				//Si regreso a 0 vuelvo a comenzar
+				AutomataLexico("", index + 1, estados[k + 1], columna, linea);
+			} else {
+				AutomataLexico(texto + programa[index], index + 1, estados[k + 1], columna, linea);
+			}
+		}
+	}
 }
 
 void main() {
@@ -41,16 +86,7 @@ void main() {
 	ReadFileLexico();
 	ReadFileTokens();
 	ReadFilePalabrasReservadas();
-
-	int estadoActual = 0;
-	for (int i = 0; i < programa.length(); i++) {
-		int* estados = SiguienteEstado(estadoActual, programa[i]);
-		cout << programa[i] << ": ";
-		for (int k = 0; k < estados[0]; k++) {
-			cout << estados[k + 1] << " ";
-		}
-		cout << endl;
-	}
+	AutomataLexico("", 0, 0, 0, 0);
 
 	system("pause");
 }
@@ -61,7 +97,6 @@ void ReadFilePalabrasReservadas() {
 	string fila;
 	if (archivo.is_open()) {
 		while (getline(archivo, fila)) {
-			istringstream SSfila(fila);
 			palabras_reservadas.push_back(fila);
 		}
 		archivo.close();
@@ -108,12 +143,21 @@ void ReadFileLexico() {
 }
 
 void ReadFileTokens() {
+	int i = 0, j = 0;
 	ifstream archivo("tokens.txt");
-	string fila;
+	string fila, celda;
 	if (archivo.is_open()) {
+		i = 0;
 		while (getline(archivo, fila)) {
 			istringstream SSfila(fila);
-			tokens.push_back(fila);
+			tokens.resize(i + 1);
+			j = 0;
+			while (getline(SSfila, celda, '\t')) {
+				if (j == 0) { if (celda != "") { tokens[i].token = celda;} else { j = 3; } }
+				if (j == 1) { tokens[i].estado = stoi(celda); }
+				j++;
+			}	
+			if(j==2){ i++; }
 		}
 		archivo.close();
 	}
