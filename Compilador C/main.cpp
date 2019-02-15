498a0f26275f5d86b0f6f33afd2791ab78ed69a1
Alexis <alexisesr73c@gmail.com>
2019-02-14 18:01:40 -0600

    Reading line per line example

--- Compilador C/main.cpp
+++ Compilador C/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
-void main() {
-	cout << "Hola";
+int main(int argc, char *argv[]){
+	freopen(argv[1], "r", stdin);
+	string line;
+	while(getline(cin,line)){
+		//Recorrer caracter por caracter de la cadena line
+		cout<<line<<"\n";
+	}
 
-	system("pause");
+	return 0;
 }
