2f658fa3716219281e37c03f513ef8dd9f0e682f
Omar Quintero <omar.quintero.ms@gmail.com>
2019-02-21 19:10:13 -0600

    Correcciones

--- Compilador C/main.cpp
+++ Compilador C/main.cpp
@@ -12,17 +12,22 @@ void ReadFileLexico();
 void ReadFilePrograma();
 void ReadFilePalabrasReservadas();
 
-struct Token { public: string token; int estado; };
+struct TablaToken { public: string token; int estado; };
+struct Identificadores { public: string identificador; string tipo; int linea; int columna; };
+struct CodigoToken { public: string token; string texto; int linea; int columna; };
 
 vector<vector<vector<int>>> matrizLexico;
-vector<Token> tokens;
+vector<TablaToken> tablaTokens;
 vector<string> palabras_reservadas;
 string programa;
 
+vector<CodigoToken> codigoTokenizado;
+vector<Identificadores> tablaIdentificadores;
+
 int* SiguienteEstado(int estadoActual, char caracter) {
 	int carac = -1;
-	carac = (caracter != '\n') ? (caracter != '\t') ? caracter - 32 + 3 : 2 : 1;
-	if ((carac >= 98)|| (carac < 0)) {  return new int[1]{ -2 }; }
+	carac = (caracter != '\0') ? (caracter != '\n') ? (caracter != '\t') ? caracter - 32 + 4 : 3: 2 : 1;
+	if ((carac >= 99)|| (carac < 0)) {  return new int[1]{ -2 }; }
 	
 	int pila = (matrizLexico[estadoActual][carac]).size();
 	int pilaVacio = (matrizLexico[estadoActual][0]).size();
@@ -38,38 +43,37 @@ int* SiguienteEstado(int estadoActual, char caracter) {
 		}
 	}
 	pilaEstados[0] = pila + pilaVacioValid;
-
 	return pilaEstados;
 }
 
 bool ValidarEstadoToken(string texto, int estado, int exitStatus) {
 	int token = -1;
 	if (exitStatus == -1) {
-		for (int i = 0; i < tokens.size(); i++) {
-			if (estado == tokens[i].estado) {
+		for (int i = 0; i < tablaTokens.size(); i++) {
+			if (estado == tablaTokens[i].estado) {
 				token = i;
-				if (tokens[i].token == "IDENTIFICADOR") { //Si es identificdor, busco si es palabra reservada
+				if (tablaTokens[i].token == "IDENTIFICADOR") { //Si es identificdor, busco si es palabra reservada
 					for (int j = 0; j < palabras_reservadas.size(); j++) {
 						if (texto == palabras_reservadas[j]) { token = 0; } //Palabra Reservada
 					}
 				}
 			}
 		}
-		if (token != -1) cout << tokens[token].token << ": " << texto << endl;
-	} else if (exitStatus == -2) {
-		cout << "NO VALID : " << texto << endl;
-	}
+		if (token != -1) cout << tablaTokens[token].token << ": " << texto << endl;
+	}	
 	return (token!=-1);
 }
 
 void AutomataLexico(string texto, int index, int estado) {
 	int* estados = SiguienteEstado(estado, programa[index]);
+	if (estados[0] == -2) {
+		cout << "ERROR: Caracter no valido: " << programa[index] << endl;
+	}
 	for (int k = 0; k < estados[0]; k++) {
-		if (index >= programa.length()-1) {	//Llegue al final, exitStatus es -1 y el estado es al que hubiera ido
-			ValidarEstadoToken(texto + programa[index], estados[k + 1], -1);
-		} else if (estados[k + 1] < 0) {			//Si el estado es menor a 0, verifico estado final y comienzo desde ahi
-			if(ValidarEstadoToken(texto, estado, estados[k + 1]))
-				AutomataLexico("", index, 0);
+		if (estados[k + 1] < 0) {			//Si el estado es menor a 0, verifico estado final y comienzo desde ahi
+			if (ValidarEstadoToken(texto, estado, estados[k + 1])) {
+				if(programa[index] != '\0') AutomataLexico("", index, 0);
+			}
 		}else {										//Si no es menor a 0, me sigo moviendo
 			if (estados[k + 1] == 0) {				//Si regreso a 0 vuelvo a comenzar
 				AutomataLexico("", index + 1, estados[k + 1]);
@@ -85,6 +89,7 @@ void main() {
 	ReadFileLexico();
 	ReadFileTokens();
 	ReadFilePalabrasReservadas();
+
 	AutomataLexico("", 0, 0);
 
 	system("pause");
@@ -110,6 +115,7 @@ void ReadFilePrograma() {
 		while (archivo.read(buffer, sizeof(buffer)))
 			programa.append(buffer, sizeof(buffer));
 		programa.append(buffer, archivo.gcount());
+		programa = programa + '\0';
 		archivo.close();
 	}
 }
@@ -149,11 +155,11 @@ void ReadFileTokens() {
 		i = 0;
 		while (getline(archivo, fila)) {
 			istringstream SSfila(fila);
-			tokens.resize(i + 1);
+			tablaTokens.resize(i + 1);
 			j = 0;
 			while (getline(SSfila, celda, '\t')) {
-				if (j == 0) { if (celda != "") { tokens[i].token = celda;} else { j = 3; } }
-				if (j == 1) { tokens[i].estado = stoi(celda); }
+				if (j == 0) { if (celda != "") { tablaTokens[i].token = celda;} else { j = 3; } }
+				if (j == 1) { tablaTokens[i].estado = stoi(celda); }
 				j++;
 			}	
 			if(j==2){ i++; }
